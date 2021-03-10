d3c3d8e2d46d7dc39e17bc306715bacf9d3ae6b2
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-10 21:27:16 +0300

    Update train.py

--- lab2/train.py
+++ lab2/train.py
@@ -23,7 +23,7 @@ for gpu in gpus:
 
 
 LOG_DIR = 'logs'
-BATCH_SIZE = 256
+BATCH_SIZE = 64
 NUM_CLASSES = 20
 RESIZE_TO = 224
 TRAIN_SIZE = 12786
@@ -63,8 +63,7 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
   inputs = tf.keras.layers.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  model = EfficientNetB0(include_top=False, weights='imagenet')
-  print(model.summary())
+  model = EfficientNetB0(include_top=False, weights='imagenet', classes=20)
   model.trainable = False
   x = model(inputs, training=False)
   x = tf.keras.layers.GlobalAveragePooling2D()(x)
