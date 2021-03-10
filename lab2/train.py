d0bae4e8f2a0eec7d6e55fd2b9d1a784ccbd304d
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-10 20:10:38 +0300

    Update train.py

--- lab2/train.py
+++ lab2/train.py
@@ -64,7 +64,7 @@ def create_dataset(filenames, batch_size):
 def build_model():
   inputs = tf.keras.layers.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
   model = EfficientNetB0(include_top=True, weights='imagenet')
-  tf.keras.layers.GlobalAveragePooling2D()(x)  
+  tf.keras.layers.GlobalAveragePooling2D()
   print(model.layers[239].rate)
   '''for layer in model.layers[:len(model.layers)-1]:
       layer.trainable = False
