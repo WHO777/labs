146f623babb81e2b6e042b47530915ce5b867a5c
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-10 13:49:49 +0300

    Update train.py

--- lab2/train.py
+++ lab2/train.py
@@ -60,7 +60,7 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
   inputs = tf.keras.layers.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  outputs = EfficientNetB0(include_top=True, weights='imagenet', classes=NUM_CLASSES)(inputs, training=False)
+  outputs = EfficientNetB0(include_top=False, weights='imagenet', classes=NUM_CLASSES)(inputs, training=False)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
 
 
