fc61d8096692fbf6512c41a1036be8c8f480c4ff
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-10 15:09:16 +0300

    transfer#1

--- lab2/train.py
+++ lab2/train.py
@@ -61,7 +61,7 @@ def create_dataset(filenames, batch_size):
 def build_model():
   inputs = tf.keras.layers.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
   model = EfficientNetB0(include_top=True, weights='imagenet', classes=20)
-  for layer model.layers[:len(model.layers)-1]:
+  for layer in model.layers[:len(model.layers)-1]:
       layers.trainable = False
   outputs = model(inputs)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
