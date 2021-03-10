8bee6c8d2564021a5c2eb81653ec2b8463ff5870
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-10 16:27:57 +0300

    Update train.py

--- lab2/train.py
+++ lab2/train.py
@@ -62,8 +62,9 @@ def build_model():
   inputs = tf.keras.layers.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
   model = EfficientNetB0(include_top=True, weights='imagenet')
   model.trainable = False
-  model.layers[239].trainable = True
-  print('\n\n\n',model.layers[-1])
+  for layer in model.layers[238:]
+      layer.trainable = True
+  #model.layers[-1].trainable = True
   x = model(inputs)
   outputs = tf.keras.layers.Dense(20)(x)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
