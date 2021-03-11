7bbb1ede442692aca406a42f9ae4de7f3dde2226
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-11 16:04:31 +0300

    Update train.py

--- lab2/train.py
+++ lab2/train.py
@@ -23,7 +23,7 @@ for gpu in gpus:
 
 
 LOG_DIR = 'logs'
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 NUM_CLASSES = 20
 RESIZE_TO = 224
 TRAIN_SIZE = 12786
@@ -80,8 +80,8 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
     inputs = tf.keras.layers.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-    x = img_augmentation(inputs)
-    model = EfficientNetB0(include_top=False, input_tensor=x, weights="imagenet")
+    #x = img_augmentation(inputs)
+    model = EfficientNetB0(include_top=False, input_tensor=inputs, weights="imagenet")
 
     # Freeze the pretrained weights
     model.trainable = False
