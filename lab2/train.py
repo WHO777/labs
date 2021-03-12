371a1fd12282e6f4bc633b87d04d33b7d46dd734
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-12 13:17:56 +0300

    Update train.py

--- lab2/train.py
+++ lab2/train.py
@@ -72,7 +72,7 @@ def build_model():
   inputs = tf.keras.layers.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
   model = EfficientNetB0(include_top=False, weights='imagenet', input_tensor=inputs)
   model.trainable = False
-  x = model.outpu
+  x = model.output
   #x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
   #x = tf.keras.layers.BatchNormalization()(x)
   #x = tf.keras.layers.Dropout(0.2)(x)
