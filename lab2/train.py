70acc432bc4831ec704f022a2e3dd11a3ad7ac89
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-15 14:08:22 +0300

    Update train.py

--- lab2/train.py
+++ lab2/train.py
@@ -76,7 +76,8 @@ def main():
   args = args.parse_args()
 
   dataset = create_dataset(glob.glob(args.train), BATCH_SIZE).shuffle(8)
-  print('\n\n', dataset.take(10))
+  for x, y in dataset[10]:
+   print(x, y)
   train_size = int(TRAIN_SIZE * 0.7 / BATCH_SIZE)
   train_dataset = dataset.take(train_size)
   validation_dataset = dataset.skip(train_size)
