52c03f701ce0bf441f9e8cae405ddaa50d687a09
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-15 11:14:34 +0300

    Update test.py

--- lab2/test.py
+++ lab2/test.py
@@ -8,26 +8,11 @@ from tensorflow.python import keras as keras
 from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.layers.experimental import preprocessing
 
-LOG_DIR = 'test_logs'
+LOG_DIR = 'logs'
 BATCH_SIZE = 256
 NUM_CLASSES = 20
 RESIZE_TO = 224
 
-
-def parse_proto_example(proto):
-  keys_to_features = {
-    'image/encoded': tf.io.FixedLenFeature((), tf.string, default_value=''),
-    'image/label': tf.io.FixedLenFeature([], tf.int64, default_value=tf.zeros([], dtype=tf.int64))
-  }
-  example = tf.io.parse_single_example(proto, keys_to_features)
-  example['image'] = tf.image.decode_jpeg(example['image/encoded'], channels=3)
-  example['image'] = tf.image.convert_image_dtype(example['image'], dtype=tf.float32)
-  example['image'] = tf.image.resize(example['image'], tf.constant([RESIZE_TO, RESIZE_TO]))
-  return example['image'], tf.one_hot(example['image/label'], depth=NUM_CLASSES)
-
-
-def normalize(image, label):
-  return tf.image.per_image_standardization(image), label
 
 def build_model():
   inputs = tf.keras.layers.Input(shape=(224, 224, 3))
@@ -52,19 +37,24 @@ def main():
   args.add_argument('--train', type=str, help='Glob pattern to collect train tfrecord files, use single quote to escape *')
   args = args.parse_args()
 
-  dataset_name = "imagewang"
-  (ds_train, ds_validation), ds_info = tfds.load(dataset_name, split=["train", "validation"], with_info=True, as_supervised=True, data_dir=args.train) 
-  size = (RESIZE_TO, RESIZE_TO)
+  ds_train = tf.keras.preprocessing.image_dataset_from_directory(
+      args.train, labels='inferred',
+       color_mode='rgb', batch_size=BATCH_SIZE, image_size=(RESIZE_TO,
+      RESIZE_TO), shuffle=True, seed=13, validation_split=0.3, subset="training"
+  )
 
+  ds_validation = tf.keras.preprocessing.image_dataset_from_directory(
+      args.train, labels='inferred',
+       color_mode='rgb', batch_size=BATCH_SIZE, image_size=(RESIZE_TO,
+      RESIZE_TO), shuffle=True, seed=13, validation_split=0.3, subset="validation"
+  )
   ds_train = ds_train.map(lambda image, label: (tf.image.resize(image, size), label))
   ds_validation = ds_validation.map(lambda image, label: (tf.image.resize(image, size), label))
 
   ds_train = ds_train.map(input_preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-  ds_train = ds_train.batch(batch_size=BATCH_SIZE, drop_remainder=True)
   ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
 
   ds_validation = ds_validation.map(input_preprocess)
-  ds_validation = ds_validation.batch(batch_size=BATCH_SIZE, drop_remainder=True)
 
   model = build_model()
   print(model.summary())
