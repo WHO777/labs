0f0278814645b110d29056ea07d3ffae61e97dc9
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-20 13:05:45 +0300

    kekw

--- lab2/train.py
+++ lab2/train.py
@@ -11,10 +11,11 @@ import glob
 import numpy as np
 import tensorflow as tf
 import time
+from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.python import keras as keras
 from tensorflow.python.keras.callbacks import LearningRateScheduler
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.layers.experimental import preprocessing
+from tensorflow.keras.preprocessing import image
+
 
 # Avoid greedy memory allocation to allow shared GPU usage
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -23,11 +24,10 @@ for gpu in gpus:
 
 
 LOG_DIR = 'logs'
-BATCH_SIZE = 256
+BATCH_SIZE = 16
 NUM_CLASSES = 20
 RESIZE_TO = 224
 TRAIN_SIZE = 12786
-
 
 def parse_proto_example(proto):
   keys_to_features = {
@@ -45,12 +45,6 @@ def normalize(image, label):
   return tf.image.per_image_standardization(image), label
 
 
-def kekw(image, label):
-  image *= 255
-  image = tf.cast(image, tf.int8)
-  #label = tf.cast(label, tf.int32)
-  return image, label
-
 def create_dataset(filenames, batch_size):
   """Create dataset from tfrecords file
   :tfrecords_files: Mask to collect tfrecords file of dataset
@@ -63,22 +57,12 @@ def create_dataset(filenames, batch_size):
     .prefetch(tf.data.AUTOTUNE)
 
 
-'''def build_model():
-  inputs = tf.keras.layers.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  outputs = EfficientNetB0(include_top=True, weights=None, classes=NUM_CLASSES)(inputs)
-  return tf.keras.Model(inputs=inputs, outputs=outputs)'''
-
-
 def build_model():
-  inputs = tf.keras.layers.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  model = EfficientNetB0(include_top=False, weights='imagenet', input_tensor=inputs)
-  model.trainable = False
-  x = tf.keras.layers.GlobalAveragePooling2D()(model.output)     
-  x = tf.keras.layers.Flatten()(x)
-  #x = tf.keras.layers.BatchNormalization()(x)
-  #x = tf.keras.layers.Dropout(0.2)(x)
-  #x = tf.keras.layers.Dense(100, activation=tf.keras.layers.ReLU())(x)
-  outputs = tf.keras.layers.Dense(NUM_CLASSES, activation="softmax")(x)
+  inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
+  model = EfficientNetB0(include_top=False,input_tensor=inputs,weights="imagenet")
+  model.trainable=False
+  x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
+  outputs = tf.keras.layers.Dense(NUM_CLASSES,activation=tf.keras.activations.softmax)(x)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
 
 
@@ -88,17 +72,13 @@ def main():
   args = args.parse_args()
 
   dataset = create_dataset(glob.glob(args.train), BATCH_SIZE)
-  for x, y in dataset.take(1):
-    print(x)
   train_size = int(TRAIN_SIZE * 0.7 / BATCH_SIZE)
   train_dataset = dataset.take(train_size)
   validation_dataset = dataset.skip(train_size)
 
   model = build_model()
-  print(model.summary())
-    
   model.compile(
-    optimizer=tf.optimizers.Adam(lr=1e-3),
+    optimizer=tf.optimizers.Adam(lr=0.001),
     loss=tf.keras.losses.categorical_crossentropy,
     metrics=[tf.keras.metrics.categorical_accuracy],
   )
