b946864b95cf5f0747de061ee47d0636ab39c864
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-25 12:21:44 +0300

    Update aug_fn.py

--- lab4/aug_fn.py
+++ lab4/aug_fn.py
@@ -4,7 +4,7 @@ class Aug_fn():
   def __init__(self):
     pass
   
-  def switch_func(name):
+  def switch_func(self, name):
      return { 
        'randomBC': A.Compose([
          A.RandomBrightnessContrast(brightness_limit=0.3, contrast_limit=0.3, p=1.0),
