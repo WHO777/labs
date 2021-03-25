203502865fa896ab3e98221e8b59a5d1fcc5a0e3
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-25 12:04:47 +0300

    Update aug_fn.py

--- lab4/aug_fn.py
+++ lab4/aug_fn.py
@@ -1,1 +1,11 @@
-print('hello world')
+import albumentation as A
+
+class aug_fn():
+  
+  def switch_func(name):
+     return { 
+       'randomBC': lambda name: A.Compose([
+         A.RandomBrightnessContrast(brightness_limit=0.3, contrast_limit=0.3, p=1.0),
+        ])
+     }.get(value)(name)
+    
