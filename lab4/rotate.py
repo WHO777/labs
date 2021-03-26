64f0f97a50648af720281c2fe98c048f8f645bd5
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-26 17:51:55 +0300

    Update rotate.py

--- lab4/rotate.py
+++ lab4/rotate.py
@@ -99,7 +99,7 @@ def main():
   sheduler = lambda epoch: 0.1 * math.exp(-0.5*epoch)
   
   for alpha in [90]:
-      for p in [0.5, 1]:
+      for p in [1]:
         transforms = A.Compose([
             A.Rotate(limit=alpha, p=p),
           ])
