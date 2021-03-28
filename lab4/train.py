4b62fa5378ab61da6cd9c60b7de31172d925190d
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-28 20:33:43 +0300

    Update train.py

--- lab4/train.py
+++ lab4/train.py
@@ -121,8 +121,8 @@ def main():
  
   sheduler = lambda epoch: 0.01 * math.exp(-0.3*epoch)
   #    for contrast in [0.2, 0.3]:
-  for brightness in [[-0.5, -0.3]]:
-    for contrast in [[1.5, 2]]:
+  for brightness in [[-0.3, 0]]:
+    for contrast in [2]:
       for p in [1]:
         #contrast = brightness
         transforms = A.Compose([
