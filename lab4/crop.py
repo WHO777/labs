fe3feed981a462115d3694a6061d72f8444b5e17
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-30 17:59:06 +0300

    Update crop.py

--- lab4/crop.py
+++ lab4/crop.py
@@ -98,7 +98,7 @@ def main():
   
   sheduler = lambda epoch: 0.01 * math.exp(-0.3*epoch)
 
-  width, height, p = 224, 224, 1
+  width, height, p = 224, 224, 0.5
   transforms = A.Compose([
       A.RandomCrop(width, height, p=p),
    ])
