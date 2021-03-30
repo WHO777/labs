1341eb05181968f5191a3798aff53eb831539047
WHO777 <61012068+WHO777@users.noreply.github.com>
2021-03-30 14:31:39 +0300

    Update crop.py

--- lab4/crop.py
+++ lab4/crop.py
@@ -42,7 +42,7 @@ def parse_proto_example(proto):
   example = tf.io.parse_single_example(proto, keys_to_features)
   example['image'] = tf.image.decode_jpeg(example['image/encoded'], channels=3)
   example['image'] = tf.image.convert_image_dtype(example['image'], dtype=tf.uint8)
-  example['image'] = tf.image.resize(example['image'], tf.constant([286, 286]), method='nearest')
+  example['image'] = tf.image.resize(example['image'], tf.constant([RESIZE_TO, RESIZE_TO]), method='nearest')
   return example['image'], tf.one_hot(example['image/label'], depth=NUM_CLASSES)
 
   
@@ -98,7 +98,7 @@ def main():
   
   sheduler = lambda epoch: 0.1 * math.exp(-0.5*epoch)
   #    for width in [0.1, 0.4, 0.6]:
-  for height in [224]:
+  for height in [200]:
       for p in [1]:
         width = height
         transforms = A.Compose([
